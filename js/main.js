707075f55100f45febcea96fb1b83dbe44514ad6
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-11-27 13:37:35 -0500

    Update JavaScript code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ class School {
   constructor(name) {
     this.name = name;
   }
+
+  toString() {
+    return this.name;
+  }
+}
+
+class Course {
+  constructor(name) {
+    this.name = name;
+  }
+
+  toString() {
+    return this.name;
+  }
 }
 
 class Person {
@@ -22,22 +36,25 @@ class Student extends Person {
 
 function main() {
   const mcgill = new School("McGill");
+  const comp251 = new Course("Comp251");
+  const ecse211 = new Course("ECSE211");
+  const ecse321 = new Course("ECSE321");
   const students = [
-    new Student("Alice", "alice.wonderland@mail.mcgill.ca", ["Comp251", "ECSE211", "ECSE321"]),
-    new Student("Bob", "bob.builder@mail.mcgill.ca", ["ECSE211"]),
+    new Student("Alice", "alice.wonderland@mail.mcgill.ca", [comp251, ecse211, ecse321]),
+    new Student("Bob", "bob.builder@mail.mcgill.ca", [ecse211]),
     new Student("Cathy", "cathy.smith@mail.mcgill.ca", [])
   ];
 
   students.forEach(student => {
     const nCourses = student.courses.length;
-    console.log(`Hi, I'm ${student.name} and I study at ${mcgill.name}!`);
+    console.log(`Hi, I'm ${student.name} and I study at ${mcgill}!`);
     if (nCourses == 0) {
       console.log("I'm taking no courses.");
     } else if (nCourses == 1) {
       console.log(`I'm taking this course: ${student.courses[0]}.`);
     } else {
       console.log("I'm taking these courses:");
-      student.courses.forEach(course => console.log(course));
+      student.courses.forEach(c => console.log(`${c}`));
     }
   });
 }
